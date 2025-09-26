632dc581d20cece87ac1f3da20d260add72e350d
KotaNch <ilacihancov@gmail.com>
2025-09-26 18:02:51 +0500

    start argument

--- hello.py
+++ hello.py
@@ -36,6 +36,19 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
+    
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    
+    if context.args:
+        param = context.args[0]
+        await update.message.reply_text(f"Its: {param}")
+    else:
+        await update.message.reply_html(
+            rf"Hi {user.mention_html()}!",
+            reply_markup=ForceReply(selective=True),
+        )
+
 
 
 def main() -> None:
@@ -45,6 +58,7 @@ def main() -> None:
     application = Application.builder().token(token).build()
 
     application.add_handler(CommandHandler("start", start))
+    
     application.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
